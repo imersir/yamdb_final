2f45538a81a4eb8628053cc656559bcefc17bef6
imersir <themersir@yandex.ru>
2021-11-12 19:17:50 +0300

    Third commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.8.5
-COPY ./ /yamdb_final
 WORKDIR /yamdb_final/
+COPY . /yamdb_final
 RUN pip install --upgrade pip && pip install -r requirements.txt
--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -55,7 +55,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
